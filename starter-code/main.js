9f3cccf4f23e5e5f37ab0cbab05af84b1c18390b
Jae Choi <Jae@blahs-MacBook-Pro.local>
2016-11-27 22:38:23 -0800

    Updated JS

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,9 @@ var cardOne = "Queen";
 var cardTwo = "Queen";
 var cardThree = "King";
 var cardFour = "King";
+
+if (cardOne === cardTwo || cardThree === cardFour) {
+	alert.("You found a match!");
+	} else if (cardOne !== cardTwo || cardThree === cardFour) { 
+		alert. ("Sorry, try again");
+}
