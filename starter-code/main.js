7d607a95db0ba3698794378c35b65787fec02288
Jae Choi <Jae@blahs-MacBook-Pro.local>
2016-11-28 01:16:24 -0800

    I did my best

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,8 +5,44 @@ var cardTwo = "Queen";
 var cardThree = "King";
 var cardFour = "King";
 
-if (cardOne === cardTwo || cardThree === cardFour) {
-	alert.("You found a match!");
-	} else if (cardOne !== cardTwo || cardThree === cardFour) { 
-		alert. ("Sorry, try again");
+var cards = ["queen", "queen", "king", "king"]
+
+var cardsInPlay = []
+
+
+
+
+/*if (cardOne === cardTwo || cardThree === cardFour) {
+	alert("You found a match!");
+} 
+else if (cardOne !== cardTwo || cardThree !== cardFour) { 
+	alert("Sorry, try again");
+}*/
+
+function createCards()
+{
+	var gameBoard = document.getElementById("game-board");
+	console.log(gameBoard);
+
+	for (var i = 0; i <= 4; i++)
+	{
+		var newCard = document.createElement('div');
+		newCard.className ='card';
+		gameBoard.appendChild(newCard);
+		newCard.setAttribute('data-card', card[i]);
+		newCard.addEventListener('click', isTwoCards)
+	}
 }
+createCards();
+
+var isMatch = function() {
+
+}
+
+function isTwoCards() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	if (cardsInPlay.length === 2) 
+	{
+		isMatch(cardsInPlay);
+	}
+};
